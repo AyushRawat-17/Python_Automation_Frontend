067d257cab89f989d4f9495df683e5bf8ebf0c0f
AyushRawat-17 <ayushrawatkv@gmail.com>
2021-06-02 02:11:13 +0530

    Heading added to Docker Component

--- src/components/DockerComponents/DockerHome.js
+++ src/components/DockerComponents/DockerHome.js
@@ -8,6 +8,7 @@ function DockerHome() {
     return (
         <div className="DockerHome">
             <img  className="DockerHome__Icon" src={dockerlogo} alt="#"/>
+            <h2 className="DockerHome__Text">Docker Configuration</h2>
             <div className="DockerHome__ButtonGroup">
                 {dockerButtonData.map(item => <DockerButton id={item.id} data={item.data}/> )}
             </div>
